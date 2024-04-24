0ce727b6df460875eb3efecf260c052ad780abb2
netdong-li <east2050@gmail.com>
2024-04-25 00:27:21 +0800

    Init axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde_json::json;
+
+async fn welcome() -> Json<serde_json::Value> {
+    Json(json!({
+        "status": "ok"
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(welcome));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
