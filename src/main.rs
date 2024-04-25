6fbf9f3ef152f8feee138e368d7371888f1ea5d2
netdong-li <east2050@gmail.com>
2024-04-25 17:46:02 +0800

    Use config port

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ async fn main() {
     let c = infrastructure::parse_config(CONFIG_FILE);
     let wire_helper = application::WireHelper::new(&c).expect("Failed to create WireHelper");
     let app = adapter::make_router(&wire_helper);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", c.app.port))
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
